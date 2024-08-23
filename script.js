3e523511ace0c3299eee365af8dc8ea172f81fa3
Fabius Gasber <fabiusgasber@gmail.com>
2024-08-23 11:50:15 -0400

    feat: add gameManager module and initialize players and get board instance

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function createPlayer(name, token) {
     return { name, token, getActive, setActive };
 }
 
+const gameManager = (function () {
+
+    const players = [
+        createPlayer('player1', 'X'),
+        createPlayer('player2', 'O')
+    ]
+
+    let activePlayer = null;
+    const board = gameboard.getBoard()
+
+    return { startGame, playRound, getPlayerTokens };
+})();
+
